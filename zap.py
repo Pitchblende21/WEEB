4947db90cd06ff64f3e5ceccffc8fed6e7780a74
Pitchblende21 <65574625+Pitchblende21@users.noreply.github.com>
2021-03-02 16:04:17 -0800

    Update zap.py

--- zap.py
+++ zap.py
@@ -1,34 +1,33 @@
+
 import os
 import platform
 import time
 import socket
 import sys
+import datetime
 
-def get_ip_address():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    print(s.getsockname()[0])
+
 
 def cd():
     cdcmd = input(">")
-    if not os.path.isdir(cdcmd):
+    if not os.path.isdir(cdcmd): 
         print('The path specified does not exist, QUITTING!')
         sys.exit()
         
 
 
-    os.chdir(cdcmd)
-    
+    os.chdir(cdcmd) 
 
+Current_Date = datetime.datetime.today().strftime ('%d-%b-%Y') 
+
+print("login at:",Current_Date)
+on = True
      
-while True:
-    a = input(">> ")
-    if a == "ifconfig":
-        get_ip_address()
+while on == True:
+    a = input(">> ") 
 
     
-        
-    elif a == "ls":
+    if a == "ls":
         for i in os.listdir('.'):
             if os.path.isfile(i):
                 print('f-',i)
@@ -39,20 +38,48 @@ while True:
             if os.path.islink(i):
                 print('l-',i)
 
-    elif a == "clear":
+    elif a == "clear": 
         for i in range(70):
             print("\n ")
 
-    elif a=="cd":
+    elif a=="cd": 
         cd()
 
-    elif a=="cd-b":
+    elif a=="cd-b": 
         os.chdir('..')
+
+    elif a=="create":  
+        createinp= input("name your file>")
+        open(createinp,"w+")
+
+    elif a=="write": 
+        fileed = input("enter file to write>> ")  
+        my_file = open(fileed, "w")
+        filetext = input('')
+        my_file.write(filetext)
+        my_file = open(fileed)
+
+        content = my_file.read()
+
+        my_file.close()
+
+        print(content)
+
+    elif a =="erase": 
+        filedel = input("file for deletion> ") 
+        os.remove(filedel)
+        print("file successfully deleted")
+
+    elif a=="read": 
+        fileread = input("file> ")
+        filereade =open(fileread, "r")
+        print(filereade.read())
 
 
 
     else:
         print("no command found.")
+
 
     
             
